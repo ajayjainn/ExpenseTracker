b78e31b3efb898bd8b0df1d825cd4a45ea73d212
Ajay <ajayjain1024@gmail.com>
2023-09-28 16:49:18 +0530

    fix: add auth routes

--- server/server.js
+++ server/server.js
@@ -8,6 +8,11 @@ import passport from 'passport'
 import userRouter from './routes/user.js'
 import passportConfig from './utils/passport.js'
 import categoryRouter from './routes/category.js'
+import { fileURLToPath } from 'url';
+import path from 'path'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express()
 
@@ -22,11 +27,17 @@ passportConfig(passport)
 
 app.use(express.static('dist'))
 
+
+
 app.use('/transactions',passport.authenticate('jwt',{session:false}),transRouter)
 
 app.use('/auth',authRouter )
 app.use('/user',userRouter )
 app.use('/category',passport.authenticate('jwt',{session:false}),categoryRouter)
+
+app.get(['/login','/register'],(req, res) =>{
+  res.sendFile(path.resolve(__dirname, './dist/index.html'));
+});
 
 app.listen(config.PORT,()=>{
   console.log('server running')
